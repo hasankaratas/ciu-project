e4c585a564b3c177cdbc2d6ed7b5d23b0274e147
hasankaratas <hasankaratas1253@gmail.com>
2023-01-21 17:15:55 +0200

    seeder-and-auth-done

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home');
+})->middleware("auth");
+
+Route::get('/', function () {
     return view('auth.login');
-});
+})->middleware("guest");
 
 Auth::routes([
     "register"=>false,
